26445b779f2385d9ec00b6881db675c00af2fa87
Inu Enike George <105659884+inuEnike@users.noreply.github.com>
2024-08-31 13:41:12 +0000

    Commit message no dey head

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const protectedRoutes = ["/", "/index.html", "/dashboard.html", "/send-coin.html", "/buy-coin.html"];
-  
-    const isAuthenticated = () => {
-      return !!localStorage.getItem("auth"); // Return true if 'auth' exists, false otherwise
-    };
-  
-    const userNotAuthenticated = () => {
-      const currentPath = window.location.pathname;
-      console.log("====================================");
-      console.log("Current Path:", currentPath);
-      console.log("Protected Routes Includes Current Path:", protectedRoutes.includes(currentPath));
-      console.log("====================================");
-      if (protectedRoutes.includes(currentPath) && !isAuthenticated()) {
-        window.location.href = "/login.html"; // Redirect to login page
-      }
-    };
-  
-    userNotAuthenticated();
-  });
-  
+  const protectedRoutes = ["/", "/index.html", "/dashboard.html", "/send-coin.html", "/settings.html", "/buy-coin.html"];
+
+  const isAuthenticated = () => {
+    return !!localStorage.getItem("auth"); // Return true if 'auth' exists, false otherwise
+  };
+
+  const userNotAuthenticated = () => {
+    const currentPath = window.location.pathname;
+    console.log("====================================");
+    console.log("Current Path:", currentPath);
+    console.log("Protected Routes Includes Current Path:", protectedRoutes.includes(currentPath));
+    console.log("====================================");
+    if (protectedRoutes.includes(currentPath) && !isAuthenticated()) {
+      window.location.href = "/login.html"; // Redirect to login page
+    }
+  };
+
+  userNotAuthenticated();
+});
+
+const logout = () => {
+  if (window.confirm("Do you want to logout?")) {
+    const token = window.localStorage.removeItem("auth")
+    let removeToken;
+    removeToken = token
+    window.location.href = "/login.html";
+  }
+
+  return removeToken
+}
